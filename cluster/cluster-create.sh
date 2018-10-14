3bf246434956b270aae80e4cfc5205e06debda86
root <root@localhost.localdomain>
2018-10-14 19:29:50 +0000

    init

---
+++ cluster/cluster-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+redis-cli --cluster create 7000 7001 7002 7003 7004 7005 --cluster-replicas 1
+
+
+
+
---
+++ cluster/shutdown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+redis-cli -p 7000 shutdown
+redis-cli -p 7001 shutdown
+redis-cli -p 7002 shutdown
+redis-cli -p 7003 shutdown
+redis-cli -p 7004 shutdown
+redis-cli -p 7005 shutdown
